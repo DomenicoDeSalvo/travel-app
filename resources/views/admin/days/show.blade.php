607ef3b479ab4e9b600d6bd9473143a905c5f87a
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-09-01 23:51:42 +0200

    Adding style to days show

--- resources/views/admin/days/show.blade.php
+++ resources/views/admin/days/show.blade.php
@@ -7,109 +7,30 @@
 @section('content')
 
 <section class="days_show my-5">
-    <div class="container">
-        <div class="row justify-content-end">
-            <div class="col-2 my-auto">
-                <button>
-                    <a href="{{ route('admin.stages.create', ['day' => $day->id]) }}">Aggiungi una tappa</a>
-                </button>
-            </div>
-            <div class="title text-center col-8 py-2">
-                <h2>{{ $day->title }}</h2>
-            </div>
-            <div class="col-2 my-auto">
-                <div class="d-flex justify-content-evenly align-items-center gap-3">
-                    <button>
-                        <a href="{{ route('admin.days.edit', $day) }}">Modifica</a>
-                    </button>
-                    <button class="bg_orange" data-bs-toggle="modal" data-bs-target="#modal-day-{{$day->id}}">Elimina</button>
-                </div>
+    <div class="container ">
+        <h2 class="title mt-4 mb-2">{{ $day->title }}</h2>
+        <h4 class="pb-3">Parte di: <a class="trip" href="{{ route('admin.trips.show', $day->trip->id) }}">{{ $day->trip->location }}</a></h4>
+        <div class="my-auto">
+            <div class="d-flex justify-content-between align-items-center gap-3 mb-3">
+                <button><a href="{{ route('admin.stages.create', ['day' => $day->id]) }}">Aggiungi una tappa</a></button>
+            <div>
+                <button><a href="{{ route('admin.days.edit', $day) }}">Modifica</a></button>
+                <button class="bg_orange" data-bs-toggle="modal" data-bs-target="#modal-day-{{$day->id}}">Elimina</button>
             </div>
         </div>
-        <div class="text-center pb-3">
-            <h4>Parte di: <a class="trip" href="{{ route('admin.trips.show', $day->trip->id) }}">{{ $day->trip->location }}</a></h4>
-        </div>
-    </div>
-
-    <div class="container">
-        <div>Data: {{ \Carbon\Carbon::parse($day->date)->format('d-m-Y') }}</div>
-        <div>
-            @if ($day->mood)
-                @if ($day->mood->id === 1)
-                    <i class="fa-regular fa-face-smile" aria-label="Happy"></i>
-                @elseif ($day->mood->id === 2)
-                    <i class="fa-regular fa-face-sad-cry" aria-label="Sad"></i>
-                @elseif ($day->mood->id === 3)
-                    <i class="fa-regular fa-face-laugh-beam" aria-label="Laugh"></i>
-                @elseif ($day->mood->id === 4)
-                    <i class="fa-regular fa-face-tired" aria-label="Tired"></i>
-                @elseif ($day->mood->id === 5)
-                    <i class="fa-regular fa-face-angry" aria-label="Angry"></i>
-                @elseif ($day->mood->id === 6)
-                    <i class="fa-regular fa-face-laugh-squint" aria-label="Laugh Squint"></i>
-                @endif
-            @endif
-        </div>
-        <div>Riassunto: {{ $day->description }}</div>
     </div>
 
     <!-- Contenitore per carosello delle tappe e le note -->
     <div class="container">
         <div class="row">
-            <!-- Colonna delle note -->
-            <div class="col-lg-3 mb-4 mb-lg-0">
-                <div class="notes-container p-3" style="height: 100%; overflow-y: auto; background-image: url({{ asset('img/postb.jpg') }}); background-size: cover; background-repeat: no-repeat; border-radius: 5px;">
-                    <h5 class="mb-3">Annotazioni del giorno</h5>
-                    <button class="btn btn-primary mb-3">
-                        <a href="{{ route('admin.notes.create', ['day' => $day->id]) }}" style="color: white;">Scrivi una nota</a>
-                    </button>
-                    @if($day->notes->isEmpty())
-                        <p>Ancora nessuna annotazione</p>
-                    @else
-                        @foreach ($day->notes as $note)
-                            <div class="note mb-3">
-                                <p>{{ $note->text }}</p>
-                                <div class="d-flex justify-content-between">
-                                    <a href="{{ route('admin.notes.edit', $note) }}" class="btn btn-secondary btn-sm">Modifica</a>
-                                    <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modal-note-{{$note->id}}">Elimina</button>
-                                </div>
-                            </div>
-
-                            {{-- MODALE ELIMINAZIONE NOTA --}}
-                            <div class="modal" id="modal-note-{{$note->id}}" tabindex="-1" aria-labelledby="modal-label-{{$day->id}}" aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Elimina Nota</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>Vuoi eliminare questa nota?</p>
-                                        </div>
-                                        <div class="modal-footer border-0">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-                                            <form action="{{ route('admin.notes.destroy', $note) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button class="btn btn-danger">Sì</button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-
             <!-- Carosello delle tappe -->
-            <div class="col-lg-9">
+            <div class="col-lg-12">
                 <div id="carouselStages{{ $day->id }}" class="carousel slide" data-bs-ride="false">
                     <div class="carousel-inner">
                         @foreach ($day->stages as $stage)
                             <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
                                 <div class="card mb-4 p-4">
-                                    <div class="card-body" style="background-image: url({{ $stage->thumb ? asset('storage/' . $stage->thumb) : asset('img/default_thumb.jpg') }}); background-size: cover; background-repeat: no-repeat;">
+                                    <div class="card-body">
                                         <h5 class="card-title">{{ $stage->title }}</h5>
                                         <p class="card-text">
                                             @if ($stage->mood)
@@ -136,6 +57,33 @@
                         <span class="visually-hidden">Next</span>
                     </button>
                 </div>
+            </div>
+        </div>
+
+        <!-- Note con scrollbar orizzontale -->
+        <div class="row mt-4">
+            <div class="col-lg-12">
+                <h5 class="mb-4">Annotazioni del giorno</h5>
+                <button class="mb-3">
+                    <a href="{{ route('admin.notes.create', ['day' => $day->id]) }}">Scrivi una nota</a>
+                </button>
+                @if($day->notes->isEmpty())
+                    <p>Ancora nessuna annotazione</p>
+                @else
+                    <div class="slides" >
+                        @foreach ($day->notes as $note)
+                            <div class="note d-inline-block mb-3 p-3">
+                                <p class="hand">{{ $note->text }}</p>
+                                <div class="d-flex justify-content-between">
+                                    <button>
+                                        <a href="{{ route('admin.notes.edit', $note) }}">Modifica</a>
+                                    </button>
+                                    <button class="bg_orange" data-bs-toggle="modal" data-bs-target="#modal-note-{{$note->id}}">Elimina</button>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     </div>
@@ -190,11 +138,36 @@
                         <p>Vuoi eliminare questa tappa?</p>
                     </div>
                     <div class="modal-footer border-0">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                        <button type="button" data-bs-dismiss="modal">No</button>
                         <form action="{{ route('admin.stages.destroy', $stage) }}" method="POST">
                             @csrf
                             @method('DELETE')
-                            <button class="btn btn-danger">Sì</button>
+                            <button class="bg_orange">Sì</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    {{-- MODALE ELIMINAZIONE NOTA --}}
+    @foreach ($day->notes as $note)
+        <div class="modal fade" id="modal-note-{{$note->id}}" tabindex="-1" aria-labelledby="modal-label-note" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Elimina Nota</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Vuoi eliminare questa nota?</p>
+                    </div>
+                    <div class="modal-footer border-0">
+                        <button type="button"  data-bs-dismiss="modal">No</button>
+                        <form action="{{ route('admin.notes.destroy', $note) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="bg_orange">Sì</button>
                         </form>
                     </div>
                 </div>
@@ -214,11 +187,11 @@
                     <p>Vuoi eliminare questa giornata?</p>
                 </div>
                 <div class="modal-footer border-0">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                    <button type="button" data-bs-dismiss="modal">No</button>
                     <form action="{{ route('admin.days.destroy', $day) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="btn btn-danger">Sì</button>
+                        <button class="bg_orange">Sì</button>
                     </form>
                 </div>
             </div>
@@ -226,5 +199,3 @@
     </div>
 </section>
 @endsection
-
-
